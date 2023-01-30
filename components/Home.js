e1a943626f07480cf380f0d0a1795c12b9db3809
Amanda <amandarb43@gmail.com>
2023-01-29 19:54:26 -0800

    added about & home component from seperate test-app

---
+++ components/Home.js
@@ -0,0 +1,13 @@
+export default function Home () {
+    return(
+        <div className="homepage">
+            <h1>Explore your tastebuds with us, Re-Sip-Ease!</h1>
+
+            <h2> We put the ease in recipes! </h2>
+            
+            <h3>With a click of a button experience culinary excellence!</h3>
+
+        </div>
+    )
+
+}
