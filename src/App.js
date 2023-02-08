b5471cf9ff891cf5a9fc400f6e6db804dfe132bd
Amanda <amandarb43@gmail.com>
2023-02-07 23:55:11 -0800

    only changed mongo uri in .env

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Pages from './components/pages/Pages';
 import Category from './components/Category';
 import { BrowserRouter } from 'react-router-dom';
 import Search from './components/Search';
-
 function App() {
   return (
   <div className="App">
